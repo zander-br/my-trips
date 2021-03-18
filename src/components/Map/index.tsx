5310ec1e06686283854e8213837df7bf236754fd
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-03-18 16:52:00 -0300

    feat: add dynimic imports

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,7 +1,11 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const Map = () => (
-  <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+  <MapContainer
+    center={[51.505, -0.09]}
+    zoom={3}
+    style={{ height: '100%', width: '100%' }}
+  >
     <TileLayer
       attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
