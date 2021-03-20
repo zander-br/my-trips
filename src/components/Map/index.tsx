356aab7e12ed4cfcc912668849540a8875587c3e
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-03-20 06:44:53 -0300

    feat: add routes to markers on map

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/dist/client/router';
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 
 type Place = {
@@ -14,21 +15,36 @@ export type MapProps = {
   places?: Place[];
 };
 
-const Map = ({ places }: MapProps) => (
-  <MapContainer
-    center={[51.505, -0.09]}
-    zoom={3}
-    style={{ height: '100%', width: '100%' }}
-  >
-    <TileLayer
-      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    {places?.map(({ id, name, location }) => {
-      const { latitude, longitude } = location;
-      return <Marker position={[latitude, longitude]} key={id} title={name} />;
-    })}
-  </MapContainer>
-);
+const Map = ({ places }: MapProps) => {
+  const router = useRouter();
+
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={3}
+      style={{ height: '100%', width: '100%' }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {places?.map(({ id, slug, name, location }) => {
+        const { latitude, longitude } = location;
+        return (
+          <Marker
+            position={[latitude, longitude]}
+            key={`place-${id}`}
+            title={name}
+            eventHandlers={{
+              click: () => {
+                router.push(`/place/${slug}`);
+              }
+            }}
+          />
+        );
+      })}
+    </MapContainer>
+  );
+};
 
 export default Map;
