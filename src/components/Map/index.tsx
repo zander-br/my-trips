fa033cb615cd824e7b7734b0dbf5f24e503d6545
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-03-21 06:18:18 -0300

    feat: add custom marker

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/dist/client/router';
 import { MapContainer, TileLayer, Marker, MapConsumer } from 'react-leaflet';
+import L from 'leaflet';
 
 import * as S from './styles';
 
@@ -34,6 +35,13 @@ const CustomTileLayer = () => {
     />
   );
 };
+
+const markerIcon = new L.Icon({
+  iconUrl: 'img/icon-192.png',
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+  popupAnchor: [0, -40]
+});
 
 const Map = ({ places }: MapProps) => {
   const router = useRouter();
@@ -72,6 +80,7 @@ const Map = ({ places }: MapProps) => {
               position={[latitude, longitude]}
               key={`place-${id}`}
               title={name}
+              icon={markerIcon}
               eventHandlers={{
                 click: () => {
                   router.push(`/place/${slug}`);
